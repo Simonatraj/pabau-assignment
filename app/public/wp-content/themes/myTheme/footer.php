d4b1fbe948abd429077fecdcfa53f480fc55a23e
Simona <trajkovskasimona10@gmail.com>
2024-08-01 00:11:41 +0200

    create a lead using jquery and ajax

--- app/public/wp-content/themes/myTheme/footer.php
+++ app/public/wp-content/themes/myTheme/footer.php
@@ -1,3 +1,37 @@
 <?php wp_footer(); ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+jQuery(document).ready(function($) {
+    $('#lead-form').on('submit', function(e) {
+        e.preventDefault();
+        let formData = {
+          api_key: $('#api_key').val(),
+          redirect_link: $('#redirect_link').val(),
+          Fname: $('#first_name').val(),
+          Lname: $('#last_name').val(),
+        }
+
+        $.ajax({
+            url: '<?php echo admin_url('admin-ajax.php'); ?>',
+            type: 'POST',
+            data: {
+                action: 'handle_lead_form',
+                data: formData
+            },
+            success: function(response) {
+                if (response.success) {
+                    alert('Lead created successfully!');
+                } else {
+                    alert('Error creating lead: ' + response.data);
+                }
+            },
+            error: function(xhr, status, error) {
+                alert('Error creating lead: ' + xhr.responseText);
+            }
+        });
+    });
+});
+</script>
 </body>
 </html>
--- app/public/wp-content/themes/myTheme/functions.php
+++ app/public/wp-content/themes/myTheme/functions.php
@@ -1,1 +1,20 @@
 <?php
+function handle_lead_form() {
+    $api_url = 'https://regression.pabau.me/OAuth2/leads/lead-curl.php';
+
+    $form_data = $_POST['data'];
+
+    $response = wp_remote_post($api_url, [
+        'method'    => 'POST',
+        'body'      => $form_data,
+    ]);
+
+    if (is_wp_error($response)) {
+        wp_send_json_error($response->get_error_message());
+    } else {
+        wp_send_json_success(wp_remote_retrieve_body($response));
+    }
+}
+
+add_action('wp_ajax_nopriv_handle_lead_form', 'handle_lead_form');
+add_action('wp_ajax_handle_lead_form', 'handle_lead_form');
